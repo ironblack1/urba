da983abbdffd5f54b6ec9419976f210165b04682
Gorky Suquinagua <gsuquinagua@hotmail.es>
2023-10-24 21:10:29 -0500

    inicio_segunda_pantalla

--- lib/pantallas/pantalla_inicio_sesion.dart
+++ lib/pantallas/pantalla_inicio_sesion.dart
@@ -11,7 +11,41 @@ class InicioSesion extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Inicio sesion'),
       ),
-      body:  Container(),
+      body: const Center(
+                      
+                   child: Column(
+                        children: [
+                                 SizedBox(height: 10),
+                                 Row(
+                                        children: [
+                                          Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                                          Text('Urbacity',
+                                                style: TextStyle(
+                                                            color: Color.fromRGBO(113, 78, 41, 1),
+                                                            fontSize: 90,
+                                                            fontFamily: 'CormorantGaramond',
+                                                            fontWeight: FontWeight.bold  
+                                                  ) 
+                                          ),
+                                        ],
+                                      ),
+                                 SizedBox(height: 5),
+                                 Row(
+                                        children: [
+                                          Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                                          Text('Te integra con tu comunidad',
+                                                style: TextStyle(
+                                                            color: Color.fromRGBO(113, 78, 41, 1),
+                                                            fontSize: 38,
+                                                            fontFamily: 'CormorantGaramond',
+                                                  ) 
+                                          ),
+                                        ],
+                                      ),
+                                 SizedBox(height: 80),
+                        ]
+      ),
+    )
     );
   }
 }
